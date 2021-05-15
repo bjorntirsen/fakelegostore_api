a84a8f2bcf2a8b8d121469a9bcca96eab2bb030a
Björn Tirsén <bjorntirsen@gmail.com>
2021-05-15 09:37:09 +0200

    :construction: Start on english version

---
+++ data_eng.php
@@ -0,0 +1,242 @@
+<?php
+
+$products_data = [
+  [
+    'id' => 1,
+    'title' => 'Colosseum',
+    'description' =>
+      'Nowhere on Earth compares to the majesty of the Colosseum of Rome. So, get ready to escape your everyday life as you take on the biggest ever LEGO® build (as at November 2020) yet. This epic 9,036-piece Colosseum model depicts each part of the famous structure in great detail. Authentic detailing shows the northern part of the outer wall’s facade and its iconic arches. The model features 3 stories, adorned with columns of the Doric, Ionic and Corinthian orders while the attic is decorated with Corinthian pilasters.',
+    'specification' =>
+      'Item: 10276,
+      Pieces: 9036,
+      Dimensions:
+      H: 11" (27cm)
+      W: 21" (52cm)
+      D: 24" (59cm),
+      ',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000782206/Lego-Creator-Colosseum-10276.jpg',
+    'price' => 549.99,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 2,
+    'title' => 'Diagongränden',
+    'description' =>
+      'Denna imponerande modell är över en meter bred och fångar verkligen den livliga, magiska shoppinggatans atmosfär. Bakom de magnifika fasaderna finns intrikata interiörer, fascinerande funktioner och välkända karaktärer. Kolla in trollstavarna hos Ollivanders™, besök Gyllenroy Lockmans boksignering i bokaffären Boklund och Alster, köp en kärleksdryck från Weasleys vassa varor och upptäck alla tillbehör och attiraljer hos Scribbulus™ skrivtillbehör, Förstklassiga quidditchtillbehör, Florean Fortescues glassbar och Daily Prophet™.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000705726/Lego-Harry-Potter-Diagon-Alley-75978.jpg',
+    'price' => 4499,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 3,
+    'title' => 'Disney slottet',
+    'description' =>
+      'För in Disneys magiska värld i ditt hem med Disney slottet. Denna mycket detaljerade LEGO® modell med över 4 000 delar ger en givande bygg- och lekupplevelse och innehåller en rad spännande funktioner på Disney-temat. Den mycket detaljerade fasaden och tornen är kopior av det klassiska Walt Disney World® Resort Askungens slott och alla rum som går att gå in i har specialfunktioner med inspiration från några av Disneys största animerade filmer. Lekmöjligheterna är oändliga med de minifigurer somingår: Musse Pigg, Mimmi Pigg, Kalle Anka, Kajsa Anka och Tingeling, och med andra karaktärer som finns i din samling.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/1709063780/Lego-Disney-The-Disney-Castle-71040.jpg',
+    'price' => 3999,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 4,
+    'title' => 'Nintendo Entertainment System™',
+    'description' =>
+      'Älskar du tv-spel? Spelade du Super Mario Bros.™ på den gamla goda tiden? Eller gillar du bara en praktisk och kreativ aktivitet på fritiden? I så fall är det nostalgiska modellsetet LEGO® Nintendo Entertainment System™ (71374) perfekt för dig. Det klossbyggda NES är packat med realistiska detaljer, inklusive ett öppningsbart fack med låsfunktion för spelkassetten och en handkontroll med kabel och kontakt.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000576813/Lego-Super-Mario-Nintendo-Entertainment-System-71374.jpg',
+    'price' => 2699,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 5,
+    'title' => 'NASA Space Shuttle Discovery',
+    'description' =>
+      'Celebrate the wonders of space with this LEGO® NASA Space Shuttle Discovery (10283) model building set for adults. With 2,354 pieces, this engaging challenge lets you build the Space Shuttle Discovery, plus the Hubble Space Telescope, launched on NASA’s STS-31 mission in 1990.',
+    'specification' =>
+      'Item: 10283,
+      Pieces: 2354,
+      Dimensions:
+      H: 9" (21cm)
+      W: 14" (34cm)
+      D: 22" (54cm),
+      ',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3001653943/Lego-NASA-Space-Shuttle-Discovery-10283.jpg',
+    'price' => 199.99,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 6,
+    'title' => 'Old Trafford - Manchester United',
+    'description' =>
+      'Manchester United fans will love building this big LEGO® set model of Old Trafford (10272) to show their allegiance to one of the world’s most famous football clubs. The building set for this LEGO football stadium provides a fun challenge to create a spectacular showpiece model.
+
+      The ultimate Manchester United gift
+      Manchester United fans can show off this Old Trafford replica at home or the office. New for February 2020, this LEGO® Creator sports set coincides with the stadium’s 110th anniversary. This building kit for adults features several evocative details including the players’ tunnel and the statue of the United Trinity.',
+    'specification' =>
+      'Item: 10272,
+      Pieces: 3898,
+      Dimensions:
+      H: 8" (19cm)
+      W: 16" (39cm)
+      D: 19" (47cm),
+      ',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000133656/Lego-Creator-Old-Trafford-Manchester-United-10272.jpg',
+    'price' => 299.99,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 7,
+    'title' => 'Ghostbusters™ ECTO-1',
+    'description' =>
+      'If you’re a Ghostbusters™ fan, we’ve got just the thing for you – the LEGO® Ghostbusters ECTO-1! Bust the stress out of everyday life and indulge in some quality me time as you build a LEGO version of the converted 1959 Cadillac Miller-Meteor ambulance from the Ghostbusters movies.
+
+      Spookily good details
+      Based on the Ghostbusters: Afterlife movie version, this ECTO-1 model features working steering, a trapdoor, ghost trap, an extending rear gunner seat, proton pack and cool details from the original car such as the iconic Ghostbusters logo.',
+    'specification' =>
+      'Item: 10274,
+      Pieces: 2352,
+      Dimensions:
+      H: 10" (23cm)
+      W: 7" (17cm)
+      D: 19" (47cm),
+      ',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000783873/Lego-Creator-Ghostbusters-ECTO-1-10274.jpg',
+    'price' => 199.99,
+    'category' => 'creator',
+  ],
+  [
+    'id' => 8,
+    'title' => 'Liebherr R 9800 grävmaskin',
+    'description' =>
+      'Gör dig redo för en kolossal LEGO® bygg- och lekupplevelse med LEGO Technic Liebherr R 9800 grävmaskin med 4 108 delar. Denna kopia, som är utvecklad i samarbete med Liebherr, styrs via den intuitiva LEGO TECHNIC CONTROL+ appen och drivs av 2 avancerade smarthubbar med 7 motorer. Den sofistikerade appteknologin möjliggör exakta rörelser och fantastiska funktioner, samtidigt som den erbjuder oändliga autentiska digitala spelkombinationer via 4 olika kontrollskärmar med cool grafik. Multifunktionskontrollen gör det möjligt att köra maskinen i alla riktningar, rotera hytten, fälla ut och höja kranarmen, öppna och luta skopan, spela upp realistiska ljudeffekter och få direkt feedback om till exempel kranarmens position, strömförbrukning och körsträcka.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/1908396539/Lego-Technic-Liebherr-R-9800-Graevmaskin-42100.jpg',
+    'price' => 5699,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 9,
+    'title' => 'Volvo 6x6 ledad lastbil',
+    'description' =>
+      'LEGO® Technic Volvo 6x6 ledad lastbil (42114) är en realistisk modellversion av Volvos största ledade lastbil och perfekt för barn som älskar byggfordon. Lastbilen drivs av 1 stor vinklad motor, 1 XL-motor och 1 L-motor med en Bluetooth-styrd smarthubb för realistisk funktionalitet.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000505032/Lego-Technic-Volvo-6x6-Articulated-Truck-42114.jpg',
+    'price' => 2799,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 10,
+    'title' => 'Porsche 911',
+    'description' =>
+      'Hylla Porsches omisskännlig stil med denna fantastiska byggutmaning när du skapar din egen klassiska samlarbil med modellsetet LEGO® Porsche 911 (10295). Välj att bygga antingen Turbo-modellen med den turboladdade motorn eller Targa-modellen med den ikoniska Targa-bågen och det avtagbara taket som förvaras under huven.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3001180357/Lego-Creator-Porsche-911-10295.jpg',
+    'price' => 1699,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 11,
+    'title' => "Dom's Dodge Charger",
+    'description' =>
+      'Ge alla som gillar Fast & Furious den ultimata upplevelsen med byggsetet LEGO® Technic Dom’s Dodge Charger (42111) för barn och vuxna. Modellen är baserad på den ikoniska Dodge Charger R/T från 1970-talet och har massor av autentiska detaljer. Den öppningsbara motorhuven avslöjar en modellversion av den klassiska V8-motorn. Andra coola funktioner som ingår är rörliga kolvar, länkarmsfjädring, styrsystem och luftfläkt. Det finns till och med nitroflaskor i bagageutrymmet för att ge liv åt Doms vågade höghastighetsrace. Precis som i verkligheten!',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000259155/Lego-Technic-Dom-s-Dodge-Charger-42111.jpg',
+    'price' => 1229,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 12,
+    'title' => 'Ducati Panigale V4 R',
+    'description' =>
+      'Stil, elegans, prestanda. Denna LEGO® Technic 42107 Ducati Panigale V4 R fångar originalets känsla och design. Det är den första modellmotorcykeln i LEGO Technic historien som har en växellåda för att utforska olika hastigheter och tekniker. Andra fantastiska funktioner som ingår är till exempel styrning, fjädring både fram och bak för realistiska rörelser och skivbromsar fram och bak.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000365251/Lego-Technic-Ducati-Panigale-V4-R-42107.jpg',
+    'price' => 749,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 13,
+    'title' => 'Porsche 911 RSR',
+    'description' =>
+      'Hylla en ikon inom ingenjörskonsten med LEGO® Technic modellen 42096 Porsche 911 RSR. Den här autentiska bilmodellen har utvecklats i samarbete med Porsche och fångar fordonets kraftfulla design med skarpa linjer och aerodynamiska detaljer, som en vinge med ”swan neck”-fästen, förlängd diffuser baktill och specialdesignade sidospeglar. Bilen har funktioner som en synlig och fungerande differentialväxel, individuell fjädring och en sexcylindrig boxermotor med rörliga kolvar framför bakaxeln.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/1871270772/Lego-Technic-Porsche-911-RSR-42096.jpg',
+    'price' => 1799,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 14,
+    'title' => 'Extrem 4X4 terrängbil',
+    'description' =>
+      'Är du redo att ta din LEGO® lekupplevelse till en ny nivå? LEGO Technic™ Extrem 4x4 terrängbil drivs av en avancerad smarthubb med 3 motorer och kontrolleras via den intuitiva LEGO TECHNIC CONTROL+ appen. Den sofistikerade appteknologin möjliggör exakta rörelser och funktioner samtidigt som den erbjuder oändliga autentiska digitala spelkombinationer med autentiska ljudeffekter. Användare kan välja mellan olika kontrollskärmar för att köra framåt, bakåt, styra, gasa, bromsa och köra över hinder.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/1903317861/Lego-Technic-4x4-X-Treme-Off-Roader-42099.jpg',
+    'price' => 2699,
+    'category' => 'technic',
+  ],
+  [
+    'id' => 15,
+    'title' => 'Robotuppfinnare',
+    'description' =>
+      'Ge dig in i den fantastiska fysiska och digitala världen med programmerbara, fjärrstyrda robotar och intelligenta skapelser. Med LEGO® MINDSTORMS® Robotuppfinnare (51515) kan unga robotfans bygga 5 unika, motoriserade robotar och fordon med den kostnadsfria appen LEGO MINDSTORMS Robotuppfinnare. De kan sedan ge liv åt en robot i taget genom att använda drag-och-släpp-programmering baserad på Scratch och slutföra roliga aktiviteter och utmanande uppdrag (en lista över kompatibla enheter finns påLEGO.com/devicecheck). Och med nästan 1 000 delar kommer barn att älska att skapa sina egna teknikleksaker och dela dem med andra robotfans på LEGO Life.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/3000646177/Lego-Mindstorms-Robot-Inventor-51515.jpg',
+    'price' => 3999,
+    'category' => 'mindstorms',
+  ],
+  [
+    'id' => 16,
+    'title' => 'Lego Mindstorms EV3',
+    'description' =>
+      'LEGO® MINDSTORMS® EV3 är här! Vi har kombinerat de oändliga möjligheterna med LEGO byggsystem och den mest avancerade tekniken någonsin. Släpp lös de kreativa krafterna hos LEGO MINDSTORMS EV3 setet och bygg och styr robotarna som kan gå, prata, tänka och göra vad som helst du kan komma på. Följ steg-för-steg-anvisningarna i 3D och bygg TRACK3R, R3PTAR, SPIK3R, EV3RSTORM samt GRIPP3R och ge dem liv med ett enkelt, intuitivt och ikonbaserat programmeringsgränssnitt. Använd fjärrkontrollen som medföljer och utför de utmanande uppdragen eller ladda ner den kostnadsfria appen och styr din robot med hjälp av din smartphone eller surfplatta. Vill du dela dina skapelser och bli inspirerad av andra?',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/1554156506/Lego-Mindstorms-EV3-31313.jpg',
+    'price' => 3399,
+    'category' => 'mindstorms',
+  ],
+  [
+    'id' => 17,
+    'title' => 'Intelligent EV3-enhet',
+    'description' =>
+      'Denna programmerbara, intelligenta enhet fungerar både som hjärta och hjärna i LEGO® MINDSTORMS® Education EV3-robotar. Den har ett upplyst gränssnitt med sex knappar som skiftar färg för att indikera enhetens aktiva status, en högupplöst svartvit display, inbyggda högtalare, en USB-port, en mini SD-minneskortläsare samt fyra indata- och fyra utdataportar. Enheten stöder också USB-, Bluetooth- och Wi-Fi-kommunikation med en dator och har ett programmeringsgränssnitt som möjliggör programmering och datauppsamling direkt i enheten.',
+    'image' =>
+      'http://www.ingeniu.com/images/catalog/p/product.EV3KIT677bri.large.jpg',
+    'price' => 2199,
+    'category' => 'mindstorms',
+  ],
+  [
+    'id' => 18,
+    'title' => 'Mindstorms EV3 Core Set',
+    'description' =>
+      'Denna kärnsats är optimerad för klassrumsanvändning och innehåller allt du behöver för att undervisa med det spännande LEGO® MINDSTORMS®-setet. Det gör det möjligt för studenter att bygga, programmera och testa sina lösningar baserat på verklig robotteknik. Den innehåller EV3 Intelligent Brick, en kraftfull liten dator som gör det möjligt att styra motorer och samla in sensoråterkoppling. Det möjliggör också BT- och WiFi-kommunikation samt tillhandahåller programmering och dataloggning. Studenter uppmuntras att brainstorma för att hitta kreativa lösningar på problem och sedan utveckla dem genom en process för att välja, bygga, testa och utvärdera dem.',
+    'image' =>
+      'https://www.pricerunner.se/product/640x640/1908137891/Lego-Mindstorm-Education-EV3-Core-Set-45544.jpg',
+    'price' => 5699,
+    'category' => 'mindstorms',
+  ],
+  [
+    'id' => 19,
+    'title' => 'Mindstorms EV3 Expansionsset',
+    'description' =>
+      'Denna uppsättning innehåller ett brett utbud av kompletterande element för att fortsätta temat kritiskt tänkande och kreativitet som presenteras i EV3 Core Set. Eleverna fördjupar sin robotikupplevelse med nya strukturella och mekaniska element och ytterligare bygginstruktioner och program.',
+    'image' =>
+      'https://pricespy-75b8.kxcdn.com/product/standard/800/2073060.jpg',
+    'price' => 1549,
+    'category' => 'mindstorms',
+  ],
+  [
+    'id' => 20,
+    'title' => 'Börja med programmering av robotik i Java med LEGO Mindstorms',
+    'description' =>
+      'Upptäck skillnaden mellan att få en robot att flytta och att få en robot att tänka. Med Mindstorms EV3 och LeJOS-ett öppen källkodsprojekt för Java Mindstorms-projekt lär du dig hur du skapar artificiell intelligens (AI) för din bot. Din robot lär dig hur man löser problem, hur man planerar och hur man kommunicerar. Längs vägen lär du dig om klassiska AI-algoritmer för att lära hårdvara hur man tänker; algoritmer som du sedan kan använda för dina egna robotinspirationer.',
+    'image' =>
+      'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQHBMtC9c0wWd7lxZgVReD7pi2mBH2Mv5zWsc1tKD9f73todaE_',
+    'price' => 719,
+    'category' => 'mindstorms',
+  ],
+];
